3231f08748fd65e0ec9d6a47ff64b5c87877ea33
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-22 18:44:05 -0500

    feat: :sparkles: models serializers definition

---
+++ blog/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import *
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
