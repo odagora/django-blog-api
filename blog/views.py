ed9274a4cc2c1213dc329e6b561e1b99dd64f2ee
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-22 18:48:23 -0500

    feat: :sparkles: models viewsets definition

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+# Viewset for Author model
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+# Viewset for Category model
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# Viewset for Post model
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# Viewset for Comment model
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
