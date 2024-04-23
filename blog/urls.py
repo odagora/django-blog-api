31feab0822746c88fc84229cbcda8af99c7abb86
Daniel <oscar.d.gonzalez@gmail.com>
2024-04-22 19:24:57 -0500

    docs: :memo: API auto-documentation setup

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
 from .views import *
+from django.urls import path
 from django.urls import include
+from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
 
 router = DefaultRouter()
 router.register(r"authors", AuthorViewSet, "authors")
@@ -11,4 +12,5 @@ router.register(r"comments", CommentViewSet, "comments")
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+    path("docs", include_docs_urls(title="Blog API")),
 ]
